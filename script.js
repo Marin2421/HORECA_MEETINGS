e4b94dd40e48a4126b56641a8408c5538e21b356
Marin2421 <164239734+Marin2421@users.noreply.github.com>
2024-10-30 17:23:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
-console.log('Script loaded');
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Script loaded');
+    
+    // Telegram WebApp ready
+    Telegram.WebApp.ready(function() {
+        console.log('Telegram WebApp is ready');
+    });
 
-Telegram.WebApp.ready(function() {
-    console.log('Telegram WebApp is ready');
-});
-
-document.getElementById('tg-button').addEventListener('click', function() {
-    console.log('Button clicked');
-    Telegram.WebApp.init();
-    console.log('Telegram WebApp initialized');
-    if (Telegram.WebApp.initDataUnsafe && Telegram.WebApp.initDataUnsafe.user) {
-        const user = Telegram.WebApp.initDataUnsafe.user;
-        console.log('User data:', user);
-        alert(`Привет, ${user.first_name}!`);
+    // Проверка наличия кнопки перед добавлением обработчика
+    const tgButton = document.getElementById('tg-button');
+    if (tgButton) {
+        tgButton.addEventListener('click', function() {
+            console.log('Button clicked');
+            Telegram.WebApp.init();
+            console.log('Telegram WebApp initialized');
+            if (Telegram.WebApp.initDataUnsafe && Telegram.WebApp.initDataUnsafe.user) {
+                const user = Telegram.WebApp.initDataUnsafe.user;
+                console.log('User data:', user);
+                alert(`Привет, ${user.first_name}!`);
+            } else {
+                console.log('Failed to get user data');
+                Telegram.WebApp.showAlert("Не удалось получить информацию о пользователе.");
+            }
+        });
     } else {
-        console.log('Failed to get user data');
-        Telegram.WebApp.showAlert("Не удалось получить информацию о пользователе.");
+        console.log('Button not found');
     }
 });
